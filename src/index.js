dd05b23652359847f6880c8b3872af9801f4452b
mario <github@lxxxvi.ch>
2019-05-09 07:21:01 +0200

    experiments

--- src/index.js
+++ src/index.js
@@ -1,28 +1,60 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { gql } from "apollo-boost";
 import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import { Query } from "react-apollo";
 
 const client = new ApolloClient({
   uri: "http://localhost:3000/graphql"
 });
 
-
-
 client
   .query({
-    query: gql`
-      { games { id } }
-    `
+    query: gql`{ games { id } }`
   })
   .then(result => console.log(result));
 
 
+class App extends React.Component {
+  render() {
+    return(
+    <ApolloProvider client={client}>
+      <div>
+        <div className="app-name">tippkick</div>
+      </div>
+      <div>
+        <h2>My first Apollo app 🚀</h2>
+      </div>
+      <div>
+        <h2>Games</h2>
+      </div>
+      <Games />
+    </ApolloProvider>
+    )
+  };
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Games = () => (
+  <Query
+    query={gql`
+      { games { id } }
+    `}
+  >
+    {({ loading, error, data }) => {
+      if (loading) return <p>Loading...</p>;
+      if (error) return <p>Error</p>;
+
+      return data.games.map((game) => (
+        <p>Game: {game.id}</p>
+      ));
+    }}
+  </Query>
+);
+
+render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
